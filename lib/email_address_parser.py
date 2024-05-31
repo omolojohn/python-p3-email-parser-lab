59e941a977cf2ad6215005cf635f0b2ccc8f8726
omolo-j <omoloj135@gmail.com>
2024-05-31 19:28:00 +0300

    email-parser-lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import re
+
+
+class EmailAddressParser:
+    def __init__(self, input_string):
+        self.input_string = input_string
+
+    def parse(self):
+        
+        parts = re.split(r'[,\s]+', self.input_string)
+        
+
+        emails = [part.strip() for part in parts if re.match(r'^[^@]+@[^@]+\.[^@]+$', part.strip())]
+        
+
+        return list(reversed(emails)) if emails else []
